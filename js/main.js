50969ce0ff6e7b7961770c3100fe1d1f9c67d490
Your Name <you@example.com>
2021-12-10 12:40:48 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let listaProduktówMlecznych = 5;
+
+console.log(listaProduktówMlecznych);
+
+let listaProduktówŚwieżych = 1;
+
+console.log(listaProduktówŚwieżych);
+
+let listaPółproduktów = 4;
+
+console.log(listaPółproduktów);
+
+let listaZakupów = [5, 1, 4,]
+
+console.log(listaZakupów);
